79c1755fbf6ef81f8cd26da2f718d92cd31b777a
BenStrawbridge <lute_midnight@yahoo.com>
2020-10-26 10:22:13 -0500

    C# Operators - Exercises 1 and 2

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static double AreaOfCircle(double r)
+        {
+            double area = r * r * 3.14159;
+
+            return area;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 17;
+            int y = 4;
+
+            int quotient = x / y;
+            int remainder = x % y;
+
+            Console.WriteLine($"{x}/{y} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Enter a radius value: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"Your area is: {area}");
+
+        }
+    }
+}
